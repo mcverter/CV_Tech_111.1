bffc0a5b54c4c5e7d91402f714add1965f469121
mcverter <mitchell.verter@gmail.com>
2014-02-23 09:38:55 -0500

    Converting to PHP

---
+++ CV_Tech_111.1.php
@@ -0,0 +1,296 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title> Mitchell Verter's Resume </title>
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+</head>
+
+<body>
+<h1> Mitchell Verter's Resume </h1>
+
+<h2> Personal </h2>
+<div id="Personal">
+    <table>
+        <thead>Links</thead>
+        <tr>
+            <td><a href="https://github.com/mcverter">Github</a> </td>
+            <td> <a href="http://www.linkedin.com/pub/mitchell-verter/88/903/778"> Linked In </a> </td>
+            <td> <a href="http://www.waste.org/~roadrunner">Personal Website</a></td>
+        </tr>
+    </table>
+
+
+
+    <table>
+        <thead>  Contact </thead>
+        <tr>
+            <td><a href="mailto:mitchell.verter@gmail.com"> mitchell.verter@gmail.com </a> </td>
+            <td> (646) 705-7784 </td>
+        </tr>
+        <tr>
+            <td> 73 St. Pauls Place, F5	/ Brooklyn, NY 11226</td>
+        </tr>
+    </table>
+
+</div>
+
+
+<h2> Past Industry Experience</h2>
+<dl>
+    <dt> <a href="http://www.curiomotion.com">Curiomotion                 Software Engineer, CurioMotion		 New York, NY				01/13-12/13
+    </a></dt>
+    <dd>Worked on a small software team that designed interactive digital signage tools using motion sensors.
+        Designed motion-sensitive Web Browser and Keyboard using Qt4, Qt5, and Kinect libraries.
+        Designed Foot Traffic Tracker and Analytics Engine, implemented as a standalone and a web application.
+        Redesigned Interactive Area object class.
+    </dd>
+    <dd> C++, QML, PHP, Javascript</dd>
+    <dd> Reasons for Leaving:
+        <ul>
+            <li>Bad Pay </li>
+            <li> <a href="http://www.curiomotion.com/#!prevas/c46c"> Bad business model</a></li>
+            <li> Bad philosophy: <a href="http://www.curiomotion.com/#!company/cm8a"> As a company, we believe "not being evil" isn't enough.
+                We think companies should actively aspire to "be good."</a></li>
+            <li> Bad code: Primary codebase littered with case statements instead of polymorphic inheritance </li>
+        </ul>
+    </dd>
+</dl>
+
+<dl>
+    <dt> <a href="http://www.google.com/patents/WO2001015004A2?cl=en">Shortcycles                  Software Engineer, ShortCycles		 San Bruno, CA				01/00-05/02
+    </a></dt>
+    <dd>
+        <ul>
+            <li>LAMP B2B based around an ontological database which allowed sales teams to mark news stories relative to their business sales cycle.  </li>
+            <li> I integrated various technologies and features into the app, including a SearchAgent to query the database periodically and send Alerts on combinations of search vectors.</li>
+        </ul>
+    </dd>
+</dl>
+
+<dl>
+    <dt> <a href="http://www.thefreelibrary.com/IKOS+Systems+to+acquire+Virtual+Machine+Works%3B+acquisition+key...-a018075037"> IKOS / Virtual Machine Works                 Software Engineer, IKOS Systems		Cambridge, MA				01/97-03/98
+    </a> </dt>
+    <dd>
+        Worked on a small software team that designed emulation software for leading microchip designers such as nVidia. Maintained and developed product.
+        Maintained and updated build, release, and installation scripts.
+        Designed object libraries such as the Expression library which evaluated expressions written in Verilog.
+        Worked with senior engineers to develop, improve, and troubleshoot their code.
+        C++, Java, PERL, shell scripting, lex/yacc, make, cvs, Verilog
+    </dd>
+</dl>
+
+
+<h2> Projects </h2>
+<dl>
+    <dt> Self Education (2014) </dt>
+    <dd> Currently studying :
+        <ul>
+            <li> Ruby On Rails and other MVC Frameworks</li>
+            <li> Angular.js, node.js, Backbone</li>
+            <li> Design Patterns </li>
+            <li> Django </li>
+            <li> Self study posted to: <a href="https://github.com/mcverter/2014_CodeStudies">https://github.com/mcverter/2014_CodeStudies</a> </li>
+        </ul>
+    </dd>
+</dl>
+
+<dl>
+    <dt> PADMA (2014) </dt>
+    <dd>
+        <p> Joint venture of CCNY Biology and CS department to create an online database of pathogen research. </p>
+        <p> I inherited a disastrous codebase and have been rewriting it  from scratch </p>
+        <p> Thus far, I have been interested primarily in code reduction. I will be using Laravel as a framework.  I have no need for any ORM or modeling of any sort, but could make use of the templating and validation features.  </p>
+        <p> PHP, HTML, CSS, Javascript, Oracle 11g</p>
+    </dd>
+</dl>
+
+
+<dl>
+    <dt> Guttersnipe (2014) </dt>
+    <dd>Converting W.A.S.T.E. Squat, Want Squat for use by the Freegan movement</dd>
+    <dd> Will use Laravel Framework because Apache Server supports PHP better than Python/Django </dd>
+</dl>
+
+<dl>
+    <dt> <a href="http://parallel-text.herokuapp.com"> Parallel Text (Spring 2013) </a> </dt>
+    <dd> Codebase: </dd>
+    <dd> Scrum Master / Coder, Parallel Text </dd>
+    <dd>
+        Led a team of student coders to create an online facing-text language-learning resource.
+        Python, Javascript, django, postgresql, CSS3, HTML5</dd>
+</dl>
+
+<dl>
+    <dt> First Row Movies (Fall 2012) </dt>
+    <dd>  Deployment:  <a href="http://waste.org/~roadrunner/firstrowmovies">http://waste.org/~roadrunner/firstrowmovies</a></dd>
+    <dd>  Codebase:  </dd>
+    <dd> Project Manager / Coder </dd>
+    <dd> Led a team of student coders to create an online movie recommendation engine.</dd>
+    <dd> Object-Oriented PHP, mysql, Javascript, CSS3, HTML5 </dd>
+</dl>
+
+
+
+<dl>
+    <dt> ScarletLetterArchives:</dt>
+    <dd>Used PERL LWP together with Flickr API to crawl and grab <a href="http://www.flickr.com/photos/17984487@N00/sets/72157594495327653/"> images</a> then compine them into a <a href="http://waste.org/~roadrunner/ScarletLetterArchives/ModernSchool/ModernSchoolByKelly.pdf"> PDF </a> which was converted to <a href="http://waste.org/~roadrunner/ScarletLetterArchives/ModernSchool/ModernSchoolByKelly.htm"> text </a></dd>
+</dl>
+
+
+<h2> Other Experience </h2>
+OTHER WORK EXPERIENCE:
+Professor/TA, York University/New School		Toronto and New York		09/07-05/12
+Awarded Jacob Javits scholarship for graduate study of Philosophy
+Taught undergraduate classes in Composition, ESL, Design Principles, and Political Philosophy
+Helped students to develop critical thinking and writing skills.
+<dl>
+    <dt>Publications</dt>
+    <dd>
+        <a href="http://www.dupress.duq.edu/products/levinas-and-asian-thought">
+            <figure>
+                <img src="http://cdn.shopify.com/s/files/1/0184/0964/products/Kalmanson_large.jpg?v=1377803946">
+                <figcaption> "The Flow of the Breath: Levinas Mouth-to-Mouth with Buddhism" (Ch 13)</figcaption>
+            </figure>
+        </a>
+    </dd>
+
+    <dd>
+        <a href="http://www.amazon.com/The-Anarchist-Turn-Jacob-Blumenfeld/dp/0745333427">
+            <figure>
+                <img src="http://bks1.books.google.com/books?id=xCWCMAEACAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70YhtJ7XfEpkXK3-6heNaXcu7ZJYRZpYT58rx5AfPRQFQBMCetRfqKZcs2xdJdnpiXJ4qndlu0g5iYNIrUlndJF6mCuB2UoO4VKPtPttp8_ESGg_MvSPH2Fc_pOoYHJg0Uk7gV9">
+                <figcaption> "Anarchism as a Practice of Caring" (Ch 7)</figcaption>
+            </figure>
+        </a>
+    <dd>
+        <a href="http://www.amazon.com/New-Perspectives-Anarchism-Nathan-Jun">
+            <figure>
+                <img src="http://ecx.images-amazon.com/images/I/41quVQj79sL._SY344_BO1,204,203,200_.jpg">
+                <figcaption> "The Anarchism of the Other Person" (Ch 5)</figcaption>
+            </figure>
+        </a>
+
+    </dd>
+
+    <dd>
+        <a href="http://www.akpress.org/dreamsoffreedommagonreader.html">
+            <figure>
+                <img src="http://www.akpress.org/media/catalog/product/cache/1/image/218x/17f82f742ffe127f42dca9de82fb58b1/d/r/dreamsoffreedommagonreader.jpg"  title="Dreams of Freedom: A Ricardo Flores Magon Reader"/>
+                <figcaption>Dreams of Freedom: A Ricardo Flores Magon Reader </figcaption>
+            </figure>
+        </a>
+    </dd>
+    <dd>
+        <a href="https://archive.org/details/ElLibroLibrePresentaTierraYLibertadLandAndLiberty">
+            <figure>
+                <img src="https://ia600800.us.archive.org/1/items/ElLibroLibrePresentaTierraYLibertadLandAndLiberty/ElLibroLibrePresentaTierraYLibertad-landAndLiberty.gif?cnt=0">
+                <figcaption>El Libro Libre presenta Tierra y Libertad: Land and Liberty, with Grammar Lessons</figcaption>
+            </figure>
+        </a>
+    </dd>
+
+
+    <h2>Lectures online</h2>
+    <dl>
+        <dt> "Undoing Patriarchy, Subverting Politics: Anarchism as a Practice of Caring" </dt>
+        <dd><iframe width="420" height="315" src="//www.youtube.com/embed/-77n1fHOvWE" frameborder="0" allowfullscreen></iframe></dd>
+    </dl>
+
+    <dl>
+        <dt> "Mitchell Verter sur Ricardo Flores Magon et la révolution mexicaine" </dt>
+        <dd>
+            <a href="http://www.cntaittoulouse.lautre.net/spip.php?article381">
+                <figure>
+                    <img src="http://www.cntaittoulouse.lautre.net/squelettes/img/bg_head_middle.jpg">
+                    <figcaption> Talk at CNT-FAI Toulouse </figcaption>
+                </figure>
+            </a>
+    </dl>
+
+
+
+
+    Author, AK Press, 				Oakland, CA			09/04-05/06
+    Co-authored 420-page volume, Dreams of Freedom:  A Ricardo Flores Magon Reader
+    Authored historical overview, translated 35 essays, created supplementary materials.
+
+    API Experience.
+    RESTFUL: Years ago, I designed a PERL LWP crawler to assemble PDFs from pages
+    hosted on Flickr.
+    Read <a href=" http://shop.oreilly.com/product/0636920030195.do"> Mining the Social Web">
+
+    <h2>Programming Experience</h2>
+
+    <ul>
+        <li>	Object Orientation
+            <ul>
+                <li> Over a decade using Object Oriented languages such as C++, Java, Python, as well as Object Oriented PERL and PHP </li>
+                <li> Know UML and other modeling diagrams </li>
+                <li> Currently studying Design Patterns   http://shop.oreilly.com/product/9780596007126.do  </li>
+
+
+
+                Portfolio Projects:
+                -------------------
+                PERL/cgi .  Moving  to Laravel
+                http://www.waste.org/~roadrunner/squat/index.html
+
+
+                LANGUAGES:
+
+
+                PHP
+                ------
+                ** I prefer the structure of object-orientation.
+                *PERL
+                **I spent many years with PERL5
+                ** Two years ShortCycles was OOPerl5,
+
+                C/C++
+                ------
+                3 years coding C/C++
+                I prefer the strictness of C.
+                C is cool
+                Took Class System V Unix commands
+
+
+                Core Java:
+                https://bitbucket.org/RoadrunnerAtWaste/securejavachat
+
+
+                PYTHON:
+                --------------
+                *Transitioned to Python from Java
+                **Quick Prototypying:  Java too byzantine
+
+                *My python is not yet pythonic.  I
+                ** Don't use lambda statements
+                ** Write for loops instead of list comprehension
+
+                Javascript is still a bit mysterious to me.  I considered it just an Object Oriented language,
+                but am now beginning to appreciate the complexity of the language .
+                http://shop.oreilly.com/product/9780596517748.do
+                Read the first chapter
+
+                MVC Experience
+                Database:
+                Mysql, Postgres, Oracle
+                oci8,
+
+
+                <!-- -
+            MITCHELL VERTER
+
+            EDUCATION	BS, Computer Science. 		City College, CUNY 		01/12-12/13 	GPA 3.9
+                BS/BA, Psych/Religion.		Brown University	 		May 2006 	GPA 3.9
+
+            SKILLS	Languages: PHP, Python, C/C++, Java, PERL, JavaScript (some Node/Angular/jquery), HTML, CSS, django/Laravel, some Ruby on Rails
+                Database: mySQL, postgresql, Oracle 11g2, some Mongo
+                Other: shell scripting, *nix utilities, Linux From Scratch, git, OOP, Design Patterns, UML.
+                Key Qualities: Team player, good communicator, quick learner, effective teacher, good leader.
+
+            SOFTWARE WORK EXPERIENCE
+            SOFTWARE PROJECTS:
+
+             -->
+</body>
+</html>
