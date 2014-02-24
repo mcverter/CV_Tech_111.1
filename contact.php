4d782a1d848933afb3f1bac89a41db954281dff2
mcverter <mitchell.verter@gmail.com>
2014-02-24 00:34:43 -0500

    fixing css

--- contact.php
+++ contact.php
@@ -3,7 +3,7 @@
 <h2> Contact </h2>
 <blockquote>
     <p>
-        <a href="mailto:mitchell.verter@gmail.com"> mitchell.verter@gmail.com </a>
+        <a target="_blank" href="mailto:mitchell.verter@gmail.com"> mitchell.verter@gmail.com </a>
         <br>
         73 St. Pauls Place, F5	/ Brooklyn, NY 11226
         <br>
@@ -16,9 +16,9 @@
 <h2> Links </h2>
 <blockquote>
     <p>
-       <a href="https://github.com/mcverter"> mcverter Github</a><br>
-       <a href="http://www.linkedin.com/pub/mitchell-verter/88/903/778"> Linked In </a><br>
-       <a href="http://www.waste.org/~roadrunner">Personal Website</a>
+       <a target="_blank" href="https://github.com/mcverter"> mcverter Github</a><br>
+       <a target="_blank" href="http://www.linkedin.com/pub/mitchell-verter/88/903/778"> Linked In </a><br>
+       <a target="_blank" href="http://www.waste.org/~roadrunner">Personal Website</a>
     </p>
 </blockquote>
 
