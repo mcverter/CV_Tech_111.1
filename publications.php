ce64f5a4d961f0eac2bf149f7243f2782fc871f9
mcverter <mitchell.verter@gmail.com>
2014-02-23 23:28:33 -0500

    adding css

--- publications.php
+++ publications.php
@@ -1,58 +1,40 @@
 <?php include("header.php");  make_header("Publications")?>
-    <ul>
-        <li>            <a href="http://www.amazon.com/Artificial-Intelligence-Century-Stephen-Lucci/dp/1936420236">
-                <figure>
-                    <img src="http://i43.tower.com/images/mm120997292/artificial-intelligence-in-21st-century-stephen-lucci-hardcover-cover-art.jpg">
-                    <figcaption> "Inductive Learning" (2nd, forthcoming)</figcaption>
-                </figure>
-            </a>
-        </li>
-
-
-        <li>
-            <a href="http://www.dupress.duq.edu/products/levinas-and-asian-thought">
-                <figure>
-                    <img src="http://cdn.shopify.com/s/files/1/0184/0964/products/Kalmanson_large.jpg?v=1377803946">
-                    <figcaption> "The Flow of the Breath: Levinas Mouth-to-Mouth with Buddhism" (Ch 13, 2014)</figcaption>
-                </figure>
-            </a>
-        </li>
-
-        <li>
-            <a href="http://www.amazon.com/The-Anarchist-Turn-Jacob-Blumenfeld/dp/0745333427">
-                <figure>
-                    <img src="http://bks1.books.google.com/books?id=xCWCMAEACAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70YhtJ7XfEpkXK3-6heNaXcu7ZJYRZpYT58rx5AfPRQFQBMCetRfqKZcs2xdJdnpiXJ4qndlu0g5iYNIrUlndJF6mCuB2UoO4VKPtPttp8_ESGg_MvSPH2Fc_pOoYHJg0Uk7gV9">
-                    <figcaption> "Anarchism as a Practice of Caring" (Ch 7, 2013)</figcaption>
-                </figure>
-            </a>
-        </li>
-
-        <li>
-            <a href="http://www.amazon.com/New-Perspectives-Anarchism-Nathan-Jun">
-                <figure>
-                    <img src="http://ecx.images-amazon.com/images/I/41quVQj79sL._SY344_BO1,204,203,200_.jpg">
-                    <figcaption> "The Anarchism of the Other Person" (Ch 5, 2009)</figcaption>
-                </figure>
-            </a>
-
-        </li>
-
-        <li>
-            <a href="http://www.akpress.org/dreamsoffreedommagonreader.html">
-                <figure>
-                    <img src="http://www.akpress.org/media/catalog/product/cache/1/image/218x/17f82f742ffe127f42dca9de82fb58b1/d/r/dreamsoffreedommagonreader.jpg"  title="Dreams of Freedom: A Ricardo Flores Magon Reader"/>
-                    <figcaption>Dreams of Freedom: A Ricardo Flores Magon Reader (2006) </figcaption>
-                </figure>
-            </a>
-        </li>
-        <li>
-            <a href="https://archive.org/details/ElLibroLibrePresentaTierraYLibertadLandAndLiberty">
-                <figure>
-                    <img src="https://ia600800.us.archive.org/1/items/ElLibroLibrePresentaTierraYLibertadLandAndLiberty/ElLibroLibrePresentaTierraYLibertad-landAndLiberty.gif?cnt=0">
-                    <figcaption>El Libro Libre presenta Tierra y Libertad: Land and Liberty, with Grammar Lessons (2002) </figcaption>
-                </figure>
-            </a>
-        </li>
-    </ul>
-
+    <div id="publications">
+        <a href="http://www.amazon.com/Artificial-Intelligence-Century-Stephen-Lucci/dp/1936420236">
+            <figure>
+                <img src="static/AI_book.jpg">
+                <figcaption> "Inductive Learning" (2nd, forthcoming)</figcaption>
+            </figure>
+        </a>
+        <a href="http://www.dupress.duq.edu/products/levinas-and-asian-thought">
+            <figure>
+                <img src="static/LandAT.png">
+                <figcaption> "The Flow of the Breath: Levinas Mouth-to-Mouth with Buddhism" (Ch 13, 2014)</figcaption>
+            </figure>
+        </a>
+        <a href="http://www.amazon.com/The-Anarchist-Turn-Jacob-Blumenfeld/dp/0745333427">
+            <figure>
+                <img src="static/ATurn.jpg">
+                <figcaption> "Anarchism as a Practice of Caring" (Ch 7, 2013)</figcaption>
+            </figure>
+        </a>
+        <a href="http://www.amazon.com/New-Perspectives-Anarchism-Nathan-Jun">
+            <figure>
+                <img src="static/NPonA.jpg">
+                <figcaption> "The Anarchism of the Other Person" (Ch 5, 2009)</figcaption>
+            </figure>
+        </a>
+        <a href="http://www.akpress.org/dreamsoffreedommagonreader.html">
+            <figure>
+                <img src="static/DoF.jpg"  title="Dreams of Freedom: A Ricardo Flores Magon Reader"/>
+                <figcaption>Dreams of Freedom: A Ricardo Flores Magon Reader (2006) </figcaption>
+            </figure>
+        </a>
+        <a href="https://archive.org/details/ElLibroLibrePresentaTierraYLibertadLandAndLiberty">
+            <figure>
+                <img src="static/TandL.gif">
+                <figcaption>El Libro Libre presenta Tierra y Libertad: Land and Liberty, with Grammar Lessons (2002) </figcaption>
+            </figure>
+        </a>
+    </div>
 <?php include("footer.php"); ?>
