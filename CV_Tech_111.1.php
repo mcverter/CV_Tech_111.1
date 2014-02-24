431110bca008209cf87fd124924f35f3d3c94c15
mcverter <mitchell.verter@gmail.com>
2014-02-23 22:53:33 -0500

    fixing css, demelza's edits

--- CV_Tech_111.1.php
+++ CV_Tech_111.1.php
@@ -2,12 +2,16 @@
 <?php include("header.php");  make_header("Main Resume")?>
 
 <ul>
-    <li> <a href="engineerWork.php"> Software Work History</a>  </li>
+    <li> <a href="engineerWork.php"> Software Developer Work History</a>  </li>
+    <li> <a href="projects.php"> Online Projects  </a> </li>
+    <li> <a href="software.php"> Software Engineering Skills </a> </li>
+    <li> <a href="demosrepos.php"> Repositories &amp; Deployments   </a></li>
+</ul>
+
+<ul>
     <li> <a href="otherWork.php"> Other Work History </a> </li>
-    <li> <a href="projects.php"> Projects  </a> </li>
     <li> <a href="publications.php"> Publications </a> </li>
     <li> <a href="lectures.php"> Lectures </a> </li>
-    <li> <a href="software.php"> Software Tookits </a> </li>
     <li> <a href="education.php"> Education </a> </li>
     <li> <a href="contact.php"> Contact </a> </li>
 
