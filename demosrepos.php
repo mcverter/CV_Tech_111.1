eaeed1cb00625c90057f7b1b6c58f5c3a52c5c20
mcverter <mitchell.verter@gmail.com>
2014-02-24 09:55:30 -0500

    adding analytics engine to portfolio

--- demosrepos.php
+++ demosrepos.php
@@ -5,6 +5,28 @@
     <li> <a target="_blank" href="https://github.com/mcverter">Github Main</a> </li>
 </ul>
 
+<h2> Analytics Viewr (Fall 2013)</h2>
+<ul>
+    <li> PHP / Javascript </li>
+    <li> (deployment, <a target="_blank" href="https://github.com/mcverter/CPlusPlus_SomePHP_AnalyticsEngine/tree/master/AnalyticsLib/WebGraph"> codebase </a>)</li>
+</ul>
+
+<li> Samples
+    <ul>
+>
+        <li> Used PHP/MySQL for "First Row Movies" (<a target="_blank" href="http://waste.org/~roadrunner/firstrowmovies/">deployment</a> / <a target="_blank" href="https://github.com/mcverter/php_mysql_firstrowmovies-github"> codebase</a>) </li>
+        <li> Used PHP/MySQL for Database class (deployment / <a target="_blank" href="https://github.com/mcverter/PHP_mysql">codebase</a>) </li>
+        <li> W.A.S.T.E. squat, want squat in PERL/CGI (<a target="_blank" href="http://waste.org/~roadrunner/squat/index.html">deployment</a> / <a target="_blank" href="https://github.com/mcverter/Guttersnipe">codebase</a>)</li>
+    </ul>
+</li>
+
+<h2> Python Image Segmentation (Spring 2013)</h2>
+<ul>
+    <li> Python, Machine Learning, Hidden Markov Models, Sphinx </li>
+    <li> <a target="_blank" href="https://github.com/mcverter/Python_Image_Segmentation"> Codebase</a> </li>
+</ul>
+
+
 <h2> Parallel Text (Spring 2013)</h2>
 <ul>
     <li> Python/Django, postgres, HTML, CSS, Javascript/jquery </li>
@@ -15,8 +37,8 @@
 <h2> First Row Movies (Fall 2012)</h2>
 <ul>
     <li> Object-Oriented PHP, mysql, Javascript, CSS3, HTML5 </li>
-    <li> <a href="http://waste.org/~roadrunner/firstrowmovies/">Deployment</a> </li>
-    <li> <a href="https://github.com/mcverter/php_mysql_firstrowmovies-github"> Codebase</a> </li>
+    <li> <a target="_blank" href="http://waste.org/~roadrunner/firstrowmovies/">Deployment</a> </li>
+    <li> <a target="_blank" href="https://github.com/mcverter/php_mysql_firstrowmovies-github"> Codebase</a> </li>
 </ul>
 
 
