19145ae5c73e73871473893279ef50f40d6b6890
mcverter <mitchell.verter@gmail.com>
2015-09-24 23:30:22 -0400

    adding resume files

--- footer.php
+++ footer.php
@@ -1,5 +1,16 @@
 </div>
 
+<div id="resumes">
+    <div>
+    Download Resume as
+    <a target="_blank" href="static/CV_Tech_2015_3.1.doc">DOC </a> or
+    <a target="_blank" href="static/CV_Tech_2015_3.1.pdf">PDF</a>
+    </div>
+    <div>
+    <a target="_blank" href="https://www.linkedin.com/pub/mitchell-verter/88/903/778">LinkedIn</a>&nbsp;&nbsp;*&nbsp;&nbsp;*&nbsp;&nbsp;
+    <a target="_blank" href="http://www.github.com/mcverter">Github</a>
+    </div>
+</div>
 <div id="email">
     Contact me :
     <a target="_blank" href="mailto:mitchell.verter@gmail.com"> mitchell.verter@gmail.com </a>
